6ac2341a3872ff229769cc1d043ac46576165441
Toy Sirichindakul <toy@192-168-1-108.tpgi.com.au>
2020-09-01 13:29:17 +1000

    new-porfolio

--- js/prevent-inspect.js
+++ js/prevent-inspect.js
@@ -1,3 +1,3 @@
-document.addEventListener('contextmenu', function(e) {
-  e.preventDefault();
-});
+// document.addEventListener('contextmenu', function(e) {
+//   e.preventDefault();
+// });
--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-   $to = “toy@kuldsigns.com“; // <– replace with your address here
-   $subject = “Test mail”;
-   $message = “Hello! This is a simple test email message.”;
-   $from = “sender@emailaddress.here“;
-   $headers = “From:” . $from;
-   mail($to,$subject,$message,$headers);
-   echo “Mail Sent.”;
-?>
